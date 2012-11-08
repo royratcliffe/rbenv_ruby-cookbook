62e88f53171e2a8748e42926516cff9dced8048d
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-11-08 07:39:06 +0000

    Apache 2.0 license

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Pioneering Software"
 maintainer_email "roy@pioneeringsoftware.co.uk"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures rbenv_ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
